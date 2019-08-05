cdf8b0880f9be8db273b686e237b7431ffaf473b
pAmbrosio <pambrosio@me.com>
2019-08-05 22:23:18 +0200

    project configuration

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const presents = [
+  [
+  "@babel/env",
+  {
+    useBuiltIns: "usage"
+  }
+  ],
+  "@babel/preset-env", "@babel/preset-react"
+];
+const plugins = [
+  [
+    "@babel/plugin-proposal-class-properties",
+    {
+      "loose": true
+    }
+  ]
+];
